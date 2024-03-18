1438b8f60e736998a0483b92f68d483dfdfaf8c5
Masego Dipela <masegolxve@gmail.com>
2024-03-18 16:29:30 +0200

    Debugging Meta Tags

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
           <meta property="og:title" content={metadata.title} />
           <meta property="og:description" content={metadata.description} />
           <meta property="og:image" content={metadata.image} />
-          <meta property="og:image:width" content="300" />
-          <meta property="og:image:height" content="300" />
+          <meta property="og:image:width" content="1200" />
+          <meta property="og:image:height" content="600" />
           <meta property="og:url" content="https://www.abibookclub.com/" />
           {/* You can add more OG tags as needed */}
           {/* Twitter Card Meta Tags */}
