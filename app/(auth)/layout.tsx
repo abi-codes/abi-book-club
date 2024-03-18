3d57998ee63d547e52c5e54c2085767df7894675
Masego Dipela <masegolxve@gmail.com>
2024-03-18 16:24:02 +0200

    Debugging Meta Tags

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,14 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
+import Head from "next/head";
+
 import "../globals.css";
 
 export const metadata = {
   title: "Abi's Book Club",
   description: "A cutting edge literature networking platform",
+  image: "/public/assets/home-page.jpeg",
 };
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,6 +21,23 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <Head>
+          <title>{metadata.title}</title>
+          <meta name="description" content={metadata.description} />
+          {/* Open Graph Meta Tags */}
+          <meta property="og:title" content={metadata.title} />
+          <meta property="og:description" content={metadata.description} />
+          <meta property="og:image" content={metadata.image} />
+          <meta property="og:image:width" content="300" />
+          <meta property="og:image:height" content="300" />
+          <meta property="og:url" content="https://www.abibookclub.com/" />
+          {/* You can add more OG tags as needed */}
+          {/* Twitter Card Meta Tags */}
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:title" content={metadata.title} />
+          <meta name="twitter:description" content={metadata.description} />
+          <meta name="twitter:image" content={metadata.image} />
+        </Head>
         <body className={`${inter.className} bg-dark-1`}>
           <div className="w-full flex justify-center items-center min-h-screen">
             {children}
